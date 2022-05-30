896a97360d82f8f2370df9975366de65c34e4395
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-05-30 18:50:39 +0200

    new button + empty formfield

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -7,7 +7,7 @@ export default function Overview() {
   const [ formData, setFormData ] =  useState(
     [
       {
-        id: 1, firstName: 'Thomas',lastName: 'Raming', position: 'General',
+        id: 1, firstName: 'Thomas', lastName: 'Raming', position: 'General',
      },
     ]
     );
@@ -24,9 +24,29 @@ export default function Overview() {
                 setFormData(newArray);
     };
 
+        function handleNew (event) {
+        event.preventDefault();
+              let newArray = [];
+              for(let x = 0; x < formData.length; x++){
+                // push old array elements in new array  
+                newArray.push(formData[x]);
+              };
+                // create new empty element
+                let newElement =       {
+                  id: newArray.length + 1, firstName: '', lastName: '', position: '',
+               };
+               newArray.push(newElement);
+              setFormData(newArray);
+        };
+
     return (
     <div className="Overview">
         <h1 className="ov-main-title">Feuerwehr-Uniform-Datenbank</h1>
+
+        <div className='mainBtn'>
+            <button type='submit' name='new' id={`new`}className='newBtn formFields' onClick={handleNew}>new</button>
+        </div>
+
         { formData.map(e =>
             <Form 
                key={e.id}  id={e.id} firstName={e.firstName} lastName={e.lastName}  position={e.position} handleChange={handleChange}
