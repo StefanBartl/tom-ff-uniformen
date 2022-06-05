5868f0cfae39db70e29b5f2e16dfdaac0d719334
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-06-05 21:27:51 +0200

    rewaerds Auth0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import Overview from "./higherOrderComponents/Overview";
+// !                 === To-Do list ===
+// TODO Login-System
+// TODO Finish: CSS for most devices & make app nice
+// TODO Finish: CSS & JS Guidelines check
+// TODO Bonus: Whats about the google drive requests? Maybe await?
+// TODO Bonus: Datenauswertung
+// TODO Bonus: Searchbar or smth
+// TODO Bonus: Adding new atributes
+// TODO Bonus: Function END-Testing!
+// TODO Why is  useEffect nullentry needed ?
+// TODO DEPLOY !!! :-)
+
 import "./styles/App.css";
+import Headline from './higherOrderComponents/Headline';
+import FirestoreDataForm from './higherOrderComponents/FirestoreDataForm';
+import Contact from './higherOrderComponents/Contact';
 
 export default function App() {
+
   return (
     <div className="App">
-      <Overview />
+        <Headline />
+        <FirestoreDataForm /> 
+        <Contact />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
 import App from './App';
-
-// import { Auth0Provider } from "@auth0/auth0-react";
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
-        {/* <Auth0Provider
-          domain="wkddatabase.eu.auth0.com"
-          clientId="51jRbnTsfujYk3tUMUnZssSWejVV1me6"
-          redirectUri={'https://stefanbartl.github.io/tom-ff-uniformen/'}
-        > */}
-
-            <App />
-      
-      {/* </Auth0Provider>, */}
-  
+              <App />
   </React.StrictMode>
 );
